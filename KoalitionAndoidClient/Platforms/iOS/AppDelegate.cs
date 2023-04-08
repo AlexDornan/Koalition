dddddc4f43fb537b9560d44b8cc80da5de3e55cd
AlexDornan <86886482+AlexDornan@users.noreply.github.com>
2023-04-08 20:33:28 +0300

    Удалил мусор

--- KoalitionAndoidClient/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ChatAppWithSignalR.Client;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
